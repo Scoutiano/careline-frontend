a59f5242b1c37d2cb43bb0f86d925854611c8efa
Scoutiano <mohanad.shuqair@gmail.com>
2022-06-28 17:53:37 +0300

    mohanad

--- src/Common/Announcements/Announcement.js
+++ src/Common/Announcements/Announcement.js
@@ -1,49 +1,138 @@
 import { Container, Card, CardGroup, Button, Row, Col } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
-import React from 'react';
-import './Announcement.css'
+import React from "react";
+import styles from "./Announcement.module.css";
 import { FaPlusCircle } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import UserProfile from "../Authentication/UserProfile";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 const Announcement = () => {
-    const eventInfo = [
-        { eventTitle: "yes", eventDetails: "B", eventDate: "2022-06-15 15:00", registeredStudents: [] },
-        { eventTitle: "yes", eventDetails: "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used ..", eventDate: "13/5/2020", registeredStudents: [] },
-        { eventTitle: "yes", eventDetails: "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...", eventDate: "13/5/2020", registeredStudents: [] },
-        { eventTitle: "yes", eventDetails: "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...", eventDate: "13/5/2020", registeredStudents: [] },
-    ];
-    const renderEvent = (event, index) => {
-        return (
-            <Col>
-                <Card key={index} >
-                    <Card.Body className='card-body-ann'>
-                        <Card.Title>{event.eventTitle}</Card.Title>
-                        <Card.Text className="text-muted overflow-hidden ">
-                            {event.eventDetails}
-                        </Card.Text>
-                        <Nav.Link href="/moreInfo">
-                            <Button variant="primary">More info</Button>
-                        </Nav.Link>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">
-                            {event.eventDate}
-                        </small>
-                    </Card.Footer>
-                </Card>
-            </Col>
-        )
+  const [redirect, setRedirect] = useState("");
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    isAuthenticated();
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data } = await axios.get("/event").catch(function (error) {
+      if (error.status === 401) {
+        setRedirect("/logout");
+      }
+    });
+    setEvents(data);
+  };
+
+  const isAuthenticated = () => {
+    console.log(UserProfile.getRole());
+    if (UserProfile.getRole() !== "STUDENT") {
+      setRedirect("/home");
+      if (UserProfile.getRole() === null) {
+        setRedirect("/login");
+      }
     }
-    return (
-        <>
-            <CardGroup>
-                <Row xs={1} md={3} classNme='g-4'>
-                    {eventInfo.map(renderEvent)}
-                </Row>
+  };
 
-                {/* "Card ms-5 shadow"  */}
+  //   const eventInfo = [
+  //     {
+  //       eventTitle: "yes",
+  //       eventDetails: "B",
+  //       eventDate: "2022-06-15 15:00",
+  //       registeredStudents: [],
+  //     },
+  //     {
+  //       eventTitle: "yes",
+  //       eventDetails:
+  //         "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used ..",
+  //       eventDate: "13/5/2020",
+  //       registeredStudents: [],
+  //     },
+  //     {
+  //       eventTitle: "yes",
+  //       eventDetails:
+  //         "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...",
+  //       eventDate: "13/5/2020",
+  //       registeredStudents: [],
+  //     },
+  //     {
+  //       eventTitle: "yes",
+  //       eventDetails:
+  //         "Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...Bootstrap cards are one of the most used bootstrap components. Cards provide an easy way to align the content with a flexible and extensible container. Cards ...",
+  //       eventDate: "13/5/2020",
+  //       registeredStudents: [],
+  //     },
+  //   ];
 
-            </CardGroup>
-        </>
-    )
-}
+  function formatDate(creationDate) {
+    var date = new Date(creationDate.substring(0, 10));
+
+    date.setFullYear(creationDate.substring(0, 4));
+    date.setMonth(creationDate.substring(5, 7) - 1);
+    date.setDate(creationDate.substring(8, 10));
+    date.setHours(creationDate.substring(11, 13));
+    date.setMinutes(creationDate.substring(14, 16));
+
+    var AMorPM = "AM";
+    var hours = date.getHours();
+
+    if (date.getHours() > 12) {
+      hours = hours - 12;
+      AMorPM = "PM";
+    }
+
+    var res =
+      date.getFullYear() +
+      "-" +
+      (date.getMonth() + 1) +
+      "-" +
+      date.getDate() +
+      " " +
+      hours +
+      ":" +
+      date.getMinutes() +
+      " " +
+      AMorPM;
+
+    return res;
+  }
+
+  const renderEvent = (event, index) => {
+    if (redirect !== "") {
+      return <Redirect to={redirect} />;
+    } else {
+      return (
+        <Col>
+          <Card key={index}>
+            <Card.Body>
+              <Card.Title>{event.eventTitle}</Card.Title>
+              <Card.Text className="text-muted">{event.eventDetails}</Card.Text>
+              <Nav.Link href={"/eventInfo?id=" + event.id}>
+                <Button variant="primary">More info</Button>
+              </Nav.Link>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">
+                {formatDate(event.eventDate)}
+              </small>
+            </Card.Footer>
+          </Card>
+        </Col>
+      );
+    }
+  };
+  return (
+    <>
+      <CardGroup>
+        <Row xs={1} md={3} className="g-4">
+          {events.map(renderEvent)}
+        </Row>
+
+        {/* "Card ms-5 shadow"  */}
+      </CardGroup>
+    </>
+  );
+};
 export default Announcement;
